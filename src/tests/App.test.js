41e428343a30bf84e333d92e1b34fc1c611a6a2e
Jorge Leris <845647@unizar.es>
2025-04-23 16:57:12 +0200

    página de inicio de sesión principal

--- src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import PrincipalLogin from '../pages/PrincipalLogin';
-
-test('renders learn react link', () => {
-  render(<PrincipalLogin />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
