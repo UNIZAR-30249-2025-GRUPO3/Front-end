20892a2773d2714f83dd6ccbee6aeb51b4d30c8a
Jorge Leris <845647@unizar.es>
2025-04-30 21:52:12 +0200

    comienzo formato pop up reservas

--- src/components/ReservationPopup.js
+++ src/components/ReservationPopup.js
@@ -11,15 +11,34 @@ const CustomPopup = ({ show, onHide }) => {
             dialogClassName="custom-modal"
             backdrop="static"
         >
-            <Modal.Header closeButton>
-                <Modal.Title>Popup</Modal.Title>
+            <Modal.Header className="position-relative">
+                <div className="position-absolute end-0 top-0 p-3">
+                    <button 
+                        type="button" 
+                        className="btn-close" 
+                        onClick={onHide} 
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <Modal.Title className="w-100 text-center">Espacio X</Modal.Title>
             </Modal.Header>
             <Modal.Body className="custom-modal-body">
                 <p>Contenido</p>
             </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={onHide}>
-                    Cerrar
+            <Modal.Footer className="d-flex justify-content-center">
+                <Button
+                    variant="outline-light" 
+                    onClick={() => {}} 
+                    style={{
+                        backgroundColor: '#000842',
+                        color: 'white',
+                        borderRadius: '10px',
+                        padding: '7px 16px',
+                        width: 'auto',
+                        minWidth: '120px'
+                    }}
+                >
+                    Reservar
                 </Button>
             </Modal.Footer>
         </Modal>
