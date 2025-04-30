3037cbafd5d8781f17aea0622c508cf2f451e114
Jorge Leris <845647@unizar.es>
2025-04-30 21:28:18 +0200

    Estilo, tamaño, centrado y scroll pop up

--- src/components/ReservationPopup.js
+++ src/components/ReservationPopup.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import '../css/CustomPopup.css';
 
 const CustomPopup = ({ show, onHide }) => {
     return (
-        <Modal show={show} onHide={onHide} centered>
+        <Modal
+            show={show}
+            onHide={onHide}
+            centered
+            dialogClassName="custom-modal"
+            backdrop="static"
+        >
             <Modal.Header closeButton>
                 <Modal.Title>Popup</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body className="custom-modal-body">
                 <p>Contenido</p>
             </Modal.Body>
             <Modal.Footer>
