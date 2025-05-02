c43be9f063a58d5fafa35c25bc39fdf8414322e3
Jorge Leris <845647@unizar.es>
2025-05-02 10:09:35 +0200

    información de los espacios

--- src/components/ReservationPopup.js
+++ src/components/ReservationPopup.js
@@ -23,7 +23,59 @@ const CustomPopup = ({ show, onHide }) => {
                 <Modal.Title className="w-100 text-center">Espacio X</Modal.Title>
             </Modal.Header>
             <Modal.Body className="custom-modal-body">
-                <p>Contenido</p>
+                <div>
+                    <h5 className="mb-4 text-center">Detalles del espacio</h5>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <table className="table table-sm table-striped table-bordered shadow-sm rounded">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Nombre</th>
+                                        <td>Aula 2.3</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Planta</th>
+                                        <td>2ª</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Capacidad</th>
+                                        <td>30 personas</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Tipo de espacio</th>
+                                        <td>Aula</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Categoría de reserva</th>
+                                        <td>Aula</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="col-md-6">
+                            <table className="table table-sm table-striped table-bordered shadow-sm rounded">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Reservable</th>
+                                        <td>Sí</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Asignado a</th>
+                                        <td>EINA</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Uso máximo permitido</th>
+                                        <td>80%</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Horario personalizado</th>
+                                        <td>No (usa el del edificio)</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </Modal.Body>
             <Modal.Footer className="d-flex justify-content-center">
                 <Button
